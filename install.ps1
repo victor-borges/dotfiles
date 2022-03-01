324966c45e57b913dad12decae8242e9acea1919
Victor Borges <victor1882@outlook.com>
2022-02-28 23:15:49 -0300

    fix: check if package is installed; remove memory usage

--- install.ps1
+++ install.ps1
@@ -1,5 +1,12 @@
-Install-Module cd-extras;
-Install-Module PSReadLine -Force;
+if (!(Get-Module -ListAvailable -Name cd-extras))
+{
+    Install-Module cd-extras;
+}
+
+if (!(Get-Module -ListAvailable -Name PSReadLine))
+{
+    Install-Module PSReadLine -Force;
+}
 
 Copy-Item -Path Microsoft.PowerShell_profile.ps1 -Destination $Profile
 Copy-Item -Path starship.toml -Destination "$HOME/.config"
