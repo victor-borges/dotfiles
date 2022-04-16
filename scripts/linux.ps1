2d9f37cf2d73bba93f06b634573d2c3cfe893e72
Victor Borges <victor1882@outlook.com>
2022-04-16 06:05:13 -0300

    fix: remove distro icons

--- scripts/linux.ps1
+++ scripts/linux.ps1
@@ -5,36 +5,6 @@ if ($env:SHELL -ne "/usr/bin/pwsh")
     chsh -s /usr/bin/pwsh
 }
 
-$env:STARSHIP_DISTRO =
-{
-    $distro = $(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
-
-    switch -Wildcard ($distro)
-    {
-        '*kali*'        { '︀ ' }
-        '*arch*'        { '︀ ' }
-        '*debian*'      { '︀ ' }
-        '*raspbian*'    { '︀ ' }
-        '*ubuntu*'      { '︀ ' }
-        '*elementary*'  { '︀ ' }
-        '*fedora*'      { '︀ ' }
-        '*coreos*'      { '︀ ' }
-        '*gentoo*'      { '︀ ' }
-        '*mageia*'      { '︀ ' }
-        '*centos*'      { '︀ ' }
-        '*opensuse*'    { '︀ ' }
-        '*tumbleweed*'  { '︀ ' }
-        '*sabayon*'     { '︀ ' }
-        '*slackware*'   { '︀ ' }
-        '*linuxmint*'   { '︀ ' }
-        '*alpine*'      { '︀ ' }
-        '*aosc*'        { '︀ ' }
-        '*nixos*'       { '︀ ' }
-        '*devuan*'      { '︀ ' }
-        '*manjaro*'     { '︀ ' }
-        '*rhel*'        { '︀ ' }
-        default         { '︀ ' }
-    }
-}
-
+# Stopped working, don't know why
+Add-Content $profile -Value '$env:STARSHIP_DISTRO = "︀ ";'
 [Environment]::SetEnvironmentVariable('STARSHIP_DISTRO', $env:STARSHIP_DISTRO, 'User')
